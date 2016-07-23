733b93afb441c10c5ab16e3e05cf85b6e4d74688
asifnavaz <asif.navaz@gmail.com>
2016-07-23 14:38:23 -0500

    adding v1

---
+++ ComplexStringManipulation/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringtoDataStructures
+{
+    public class Node
+    {
+        public Node Parent;
+        public string strValue;
+        public List<Node> listSubnodes;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="strlevel"></param>
+        public static void PrintNode(Node node, string strlevel)
+        {
+            if (node.strValue.Length > 0)
+            {
+                Console.WriteLine(strlevel + "-" + node.strValue);
+            }
+               
+            foreach (Node n in node.listSubnodes)
+            {
+                PrintNode(n, strlevel + "  ");
+            }
+        }
+
+    }
+}
---
+++ ComplexStringManipulation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringtoDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strInput = "(id,created,employee(id,firstname,employeeType(id),lastname),location)";
+
+            Node root = new Node { Parent = null, strValue = "root", listSubnodes = new List<Node>() };
+            Node node = root;
+
+            bool escape = false;
+            foreach (char c in strInput)
+            {
+                if(escape)
+                {
+                    node.strValue += c;
+                    escape = false;
+                }
+                else
+                {
+                    switch (c)
+                    {
+                        case '{':
+                            node = new Node { Parent = node, strValue = String.Empty, listSubnodes = new List<Node>() };
+                            node.Parent.listSubnodes.Add(node);
+                            break;
+                        case '}':
+                            node = new Node { Parent = node, strValue = String.Empty, listSubnodes = new List<Node>() };
+                            if(node.Parent!=null)
+                            {
+                                node.Parent.listSubnodes.Add(node);
+                            }
+                            break;
+
+                        case '\\':
+                            escape = true;
+                            break;
+                        default:
+                            node.strValue += c;
+                            break;
+                    }
+                }
+            }
+
+
+            Node.PrintNode(root,string.Empty);
+
+        }
+    }
+
+}
